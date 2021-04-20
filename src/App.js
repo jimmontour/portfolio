4c3ca46f7b914561a54520c08b8de5a4fe3ee6af
Jim Montour <jimmontour@gmail.com>
2021-04-19 21:50:04 -0400

    fix Footer bug

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import "normalize.css";
 
 import Nav from './components/Nav';
 import Hero from "./components/home/Hero";
-import About from './components/home/About'
+import About from "./components/home/About";
 import Projects from "./components/home/Projects";
 import Skills from "./components/home/Skills";
 import Contact from "./components/home/Contact";
-import Footer from './components/Footer'
+import Footer from "./components/Footer"
 
 function App() {
   return (
